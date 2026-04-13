aa6e8845f7a59985baa64aceed72b4d84a2aef1b
GitHub Action <action@github.com>
2026-04-13 22:40:20 +0000

    Update flight data Mon Apr 13 22:40:20 UTC 2026

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const flightInfo = {
-    "lastUpdate": "2026-04-14 05:32:38",
-    "version": 1776144758,
+    "lastUpdate": "2026-04-14 05:40:19",
+    "version": 1776145219,
     "data": [
         {
             "type": "출발",
@@ -16,7 +16,7 @@ const flightInfo = {
             "timestamp": 1776129900,
             "flight": "SU836",
             "city": "Yekaterinburg",
-            "status": "도착예정 (07:23)"
+            "status": "도착예정 (07:25)"
         },
         {
             "type": "도착",
@@ -24,7 +24,7 @@ const flightInfo = {
             "timestamp": 1776133800,
             "flight": "SU294",
             "city": "Moscow",
-            "status": "도착예정 (08:50)"
+            "status": "도착예정 (08:53)"
         },
         {
             "type": "도착",
@@ -32,7 +32,7 @@ const flightInfo = {
             "timestamp": 1776134700,
             "flight": "VN441",
             "city": "인천",
-            "status": "도착예정 (09:40)"
+            "status": "도착예정 (09:37)"
         },
         {
             "type": "출발",
@@ -160,7 +160,7 @@ const flightInfo = {
             "timestamp": 1776153300,
             "flight": "WZ3201",
             "city": "Moscow",
-            "status": "도착예정 (14:47)"
+            "status": "도착예정 (14:45)"
         },
         {
             "type": "도착",
@@ -176,7 +176,7 @@ const flightInfo = {
             "timestamp": 1776155400,
             "flight": "B2795",
             "city": "Minsk",
-            "status": "도착예정 (15:19)"
+            "status": "도착예정 (15:21)"
         },
         {
             "type": "출발",
