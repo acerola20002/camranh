3500ee0bd7dbc20018ac351aa9621a0a7fa468c2
GitHub Action <action@github.com>
2026-04-15 09:40:20 +0000

    Update flight data Wed Apr 15 09:40:20 UTC 2026

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const flightInfo = {
-    "lastUpdate": "2026-04-15 16:30:24",
-    "version": 1776270624,
+    "lastUpdate": "2026-04-15 16:40:19",
+    "version": 1776271219,
     "data": [
         {
             "type": "도착",
@@ -16,7 +16,7 @@ const flightInfo = {
             "timestamp": 1776252900,
             "flight": "3U3939",
             "city": "청두",
-            "status": "도착예정 (18:09)"
+            "status": "도착예정 (18:14)"
         },
         {
             "type": "출발",
@@ -40,7 +40,7 @@ const flightInfo = {
             "timestamp": 1776260400,
             "flight": "BX787",
             "city": "인천",
-            "status": "도착예정 (20:26)"
+            "status": "도착예정 (20:24)"
         },
         {
             "type": "출발",
