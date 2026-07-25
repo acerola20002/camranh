6ea9f210550a8242d268f3935e2ae43ebda1d9c1
GitHub Action <action@github.com>
2026-07-25 19:49:43 +0000

    Update flight data Sat Jul 25 19:49:43 UTC 2026

--- data.js
+++ data.js
@@ -1,30 +1,14 @@
 const flightInfo = {
-    "lastUpdate": "2026-07-26 01:33:03",
-    "version": 1785029583,
+    "lastUpdate": "2026-07-26 02:49:42",
+    "version": 1785034182,
     "data": [
-        {
-            "type": "출발",
-            "time": "07/26 01:35",
-            "timestamp": 1785004500,
-            "flight": "7C2304",
-            "city": "인천",
-            "status": "탑승중"
-        },
-        {
-            "type": "출발",
-            "time": "07/26 01:45",
-            "timestamp": 1785005100,
-            "flight": "TW46",
-            "city": "청주",
-            "status": "곧 출발"
-        },
         {
             "type": "출발",
             "time": "07/26 03:10",
             "timestamp": 1785010200,
             "flight": "VJ918",
             "city": "부산",
-            "status": "출발예정"
+            "status": "곧 출발"
         },
         {
             "type": "도착",
@@ -32,7 +16,7 @@ const flightInfo = {
             "timestamp": 1785030000,
             "flight": "C65537",
             "city": "Tashkent",
-            "status": "도착예정 (08:33)"
+            "status": "도착예정 (08:44)"
         },
         {
             "type": "도착",
@@ -48,7 +32,7 @@ const flightInfo = {
             "timestamp": 1785032400,
             "flight": "KC157",
             "city": "Astana",
-            "status": "도착예정 (08:57)"
+            "status": "도착예정 (08:42)"
         },
         {
             "type": "도착",
@@ -64,7 +48,7 @@ const flightInfo = {
             "timestamp": 1785033000,
             "flight": "SU294",
             "city": "Moscow",
-            "status": "도착예정 (09:42)"
+            "status": "도착예정 (09:33)"
         },
         {
             "type": "도착",
