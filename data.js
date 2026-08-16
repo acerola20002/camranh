0c90e4837b862a7ca4fc8b9af00a569de6023794
GitHub Action <action@github.com>
2026-08-16 20:45:12 +0000

    Update flight data Sun Aug 16 20:45:12 UTC 2026

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const flightInfo = {
-    "lastUpdate": "2026-08-17 03:20:53",
-    "version": 1786936853,
+    "lastUpdate": "2026-08-17 03:45:11",
+    "version": 1786938311,
     "data": [
         {
             "type": "도착",
@@ -40,7 +40,7 @@ const flightInfo = {
             "timestamp": 1786933800,
             "flight": "SU294",
             "city": "Moscow",
-            "status": "도착예정 (09:29)"
+            "status": "도착예정 (09:33)"
         },
         {
             "type": "도착",
@@ -184,7 +184,7 @@ const flightInfo = {
             "timestamp": 1786950600,
             "flight": "SU298",
             "city": "Moscow",
-            "status": "예정"
+            "status": "도착예정 (13:21)"
         },
         {
             "type": "출발",
